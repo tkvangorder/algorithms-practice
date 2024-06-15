75314c00242642730a50f0c9db77cab4f1c88f6b
sullis <github@seansullivan.com>
2024-06-14 17:06:16 -0700

    add IdentifyLettersInArray

---
+++ src/main/java/algorithms/arrays/IdentifyLettersInArray.java
@@ -0,0 +1,38 @@
+package algorithms.arrays;
+
+/**
+
+ Implement a function that accepts an array as input.
+
+ The input array can contain characters and nested arrays.
+ Each nested array can contain characters and nested arrays.
+
+ The function returns an array of characters. The characters
+ must appear in the same order that they appear in the input array.
+
+ The output array will have no duplicate characters.
+ Characters should be treated case insensitively.
+
+ Example:
+   input = [ 'm', [ 'a', 'p', 'p', 'l', [ 'e' ] ], 's' ]
+   output = [ 'm', 'a', 'p', 'l', 'e', 's' ]
+
+ Example:
+    input = [ 'a', 'A' ]
+    output = [ 'a' ]
+
+ Example:
+    input = [ [ 'x' ], [ 'A', 'a' ], [ 'X' ] ]
+    output = [ 'x', 'A' ]
+
+ Example:
+    input = [ 'Z' ]
+    output = [ 'Z' ]
+
+ */
+public class IdentifyLettersInArray {
+
+  public static void main(String[] args) {
+  }
+
+}
