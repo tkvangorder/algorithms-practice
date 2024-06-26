7ee70c2ce3409cb9cea881ccf6eede617379fec1
sullis <github@seansullivan.com>
2024-06-26 15:25:19 -0700

    add ExpressionParser.java

---
+++ src/main/java/algorithms/strings/ExpressionParser.java
@@ -0,0 +1,31 @@
+package algorithms.strings;
+
+/*
+
+Build an expression parser.
+The format of an individual statement will be:
+“( CMD VAL VAL )”
+
+VAL can be an integer or another statement.
+
+You can assume valid inputs for this entire exercise.
+If you need to, clarify what you assume are valid inputs.
+
+The first two commands to implement are ADD and MULT,
+which add and multiply the values, respectively.
+
+Example:
+
+parse("( ADD 3 4 )") == 7
+
+parse("( ADD ( MULT 1 2 ) ( ADD 3 4 ) )") = 9
+
+ */
+
+public class ExpressionParser {
+
+    public static void main(String[] args) {
+        // todo
+    }
+}
+
